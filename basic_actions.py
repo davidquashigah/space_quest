dec62b2e1f0912d3fe32b2667f3fcd73ef7a4fc6
davidquashigah <68356648+davidquashigah@users.noreply.github.com>
2020-07-21 00:14:55 -0400

    Update basic_actions.py
    The multiple-choice works. The User can input. However, the results are not logical. Must check over.

--- basic_actions.py
+++ basic_actions.py
@@ -1,16 +1,19 @@
-def basic_actions:
+# this is a demonstration of the multiple choice questionnaire
 
-# prompt the user with possible
 while True:
-    basic_action = input("Do you want to:\n A) Attack\n B) Runaway \n C) Negotiate?\n [A/B/C]: ")
-    if basic_action in ['A', 'B', 'C']:
+    basic_action = input("Do you want to:\n "
+                         "A) Attack \n "
+                         "B) Runaway \n "
+                         "C) Negotiate\n "
+                         "[A/B/C]: ")
+    if basic_action in ['A', 'a', 'b', 'B', 'c', 'C']:
         break
-if basic_action == "A":
-    print("You choose to attack!")
-if basic_action == "B":
-    print("You attempt to runaway!")
-elif basic_action == "C":
-    print("You attempt to negotiate.")
 
-# create functions for each basic action
-# Attack power =  strength_points * weapon_points
+if basic_action == "a" or "A":
+    print("You choose to attack!")
+
+elif basic_action == "b" or "B":
+    print("You attempt to runaway!")
+
+else:
+    print("You attempt to negotiate.")
