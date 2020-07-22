a3e3028d94b4e8acfc5d1b73c53b0c86278daec3
davidquashigah <68356648+davidquashigah@users.noreply.github.com>
2020-07-22 12:13:15 -0400

    update to the intro to the game
    I cleaned up the print. as well, I clarified the class choice function.

--- intro.py
+++ intro.py
@@ -14,62 +14,48 @@ def intro():
         character_name = input("What is the name of your explorer? ")
         print("Welcome to Space Quest, " + character_name + "!")
 
-    print("Next, it is time to choose your explorer.")
-    print("Each explorer has their own strengths, weakness, and skills.")
-
-    print("There are three types of explorers: Marines, Teeps, and Rangers.")
-
-    print("Which explorer would you like to read about?")
+    print("Next, it is time to choose your explorer. \n"
+          "Each explorer has their own strengths, weakness, and skills. \n"
+          "There are three types of explorers: Marines, Teeps, and Rangers. \n"
+          "Which explorer would you like to read about?")
 
     marine_description = input("Press 'm' or 'M' to read about a short description on the Marines: ")
     if marine_description in ['m', 'M']:
-        print("The marines are hard-nosed veterans of the Chrysalis war. "
-              "They are comfortable in close-quarters combat. "
-              "Their primary weapons include shotgun-axes, dual-swords, and "
-              "proton-cannons.  Although they have the highest durability, "
-              "their range is limited and so is their agility. You will find "
-              "marines behind enemy lines. Are you ready to join the Marines?")
+        print("The marines are hard-nosed veterans of the Chrysalis war. \n"
+              "They are comfortable in close-quarters combat. \n"
+              "Their primary weapons include shotgun-axes, dual-swords, and proton-cannons. \n"
+              "Although they have the highest durability, their range is limited and so is their agility. \n"
+              "You will find marines behind enemy lines. \n"
+              "Are you ready to join the Marines?")
 
     teep_description = input("Press 't' or 'T' to read about a short description on the Teeps: ")
     if teep_description in ['t', 'T']:
-        print("Teeps (or telepaths) are seldom found within the Space "
-              "Exploration Foundation (SEF). Teep civil rights have grown "
-              "steadily as Alien Space Federations use teeps for alien-alien "
-              "communication. While low-level teeps can determine the colour "
-              "in a person’s mind, high-level teeps can perform brain "
-              "surgery, read the future, and mind-control. Are you mindful?")
+        print("Teeps (or telepaths) are seldom found within the Space xploration Foundation (SEF). \n"
+              "Teep civil rights have grown as Alien Space Federations use Teeps for alien-to-alien communication. \n"
+              "While low-level Teeps can determine what a person is thinking, high-level teeps can perform brain surgery, read the future, and mind-control. \n"
+              "Are you mindful enough to join the Teeps?")
 
     ranger_description = input("Press 'r' or 'R' to read about a short description on the Rangers: ")
     if ranger_description in ['r', 'R']:
-        print("Rangers are never seen, heard, but felt. From miles away, "
-              "rangers are excellent in long-range battle. Their greatest "
-              "weapon is their sniper, and cover. Rangers and Marines are "
-              "always in competition to see which is the dominant military "
-              "class. Rangers are the only class that use animals to aid "
+        print("Rangers are never seen, heard, but felt. \n"
+              "From miles away, rangers are excellent in long-range battle. \n"
+              "Their greatest weapon is their sniper rifle, cover, and beast. \n"
+              "Rangers and Marines are always in competition to see which is the dominant military class. \n"
+              "Rangers are the only class that use beast to aid them in their quests. \n"
+              "Are you prepared to leave the bush, and join the Rangers?")
               "them in their quests.")
+            
+    while True:
+        class_choice = input("So which space explorer will you be? A Marine, a Teep, or a Ranger? [M/T/R] ")
 
-    print("Did you get all that?")
+        if class_choice in ['M', 'm', 'T', 't', 'R', 'r']:
+            break
 
-    print("So which space explorer will you be? A Marine, a Teep, or a Ranger?")
-
-# Work on choosings hero class
-
-    class_choice = input("Press 'm' or 'M' to enroll in the Marine Academy: "
-    "press 't' or 'T' to join the Intergalactic Teep Alliance: "
-    "press 'r' or 'R' to unite with your animal friends among the Rangers: ")
-
-    class_choice = ()
     if class_choice in ['m', 'M']:
         print("You are enrolled in Marine Academy " + character_name + "!")
-    if class_choice in ['t', 'T']:
-        print("You joined the Intergalactic Teep Alliance " + character_name + "!")
-    if class_choice in ['r', 'R']:
-        print("Your are now among animals as a Ranger " + character_name + "!")
-    else:
-        input("Press 'm' or 'M' to enroll in the Marine Academy: "
-    "press 't' or 'T' to join the Intergalactic Teep Alliance: "
-    "press 'r' or 'R' to unite with your animal friends among the Rangers: ")
-
-
+    elif class_choice in ['t', 'T']:
+        print("You joined the Intergalactic Teep Alliance, " + character_name + "!")
+    elif class_choice in ['r', 'R']:
+        print("Your are now among animals as a Ranger, " + character_name + "!")
 
 intro()
