5174f84d61a37a79506dde1e3f795b4029f137e6
davidquashigah <68356648+davidquashigah@users.noreply.github.com>
2020-08-02 11:53:42 -0400

    Update attack.py
    organized the code within the function

--- attack.py
+++ attack.py
@@ -6,19 +6,17 @@ max = 10 # maximum roll
 
 roll_die = ['Y', 'y']
 
-while roll_die == 'y' or 'Y':
-    print("Rolling the dice . . .")
-    print("You rolled a . . . ")
-    roll_result = (random.randint(min, max)) # the roll_result determines the User's attack power, runaway success, and negotative effectiveness.
-    print(roll_result)
-    break
+def attack(str_points, wpn_atk):
+    while roll_die == 'y' or 'Y':
+        print("Rolling the dice . . .")
+        print("You rolled a . . . ")
+        roll_result = (random.randint(1, 10)) # the roll_result determines the User's attack power, runaway success, and negotative effectiveness.
+        print(roll_result)
+        break
 
-str_points = 10 # character strength points
-wpn_atk = 2 # weapon attack damage
+    max_atk_dmg = str_points * wpn_atk
+    print(max_atk_dmg)
+    atk_dmg = max_atk_dmg * int(roll_result) / 10
+    print("You did " + str(int(atk_dmg)) + " points of damage!")
 
-# below is a demonstration of the attack scenario, using a 10-sided die.
-max_atk_dmg = str_points * wpn_atk
-print(max_atk_dmg)
-
-atk_dmg = max_atk_dmg * int(roll_result) / 10
-print("You did " + str(int(atk_dmg)) + " points of damage!")
+attack(10,2)
